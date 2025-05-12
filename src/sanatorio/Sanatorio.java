e1a73ffbba14c2c6e44b1720f65265bfb59a3f2e
LeanVega <tomasvegalencina@gmail.com>
2025-05-12 00:04:16 -0300

    Commit 3

--- src/sanatorio/Sanatorio.java
+++ src/sanatorio/Sanatorio.java
@@ -4,9 +4,9 @@ public class Sanatorio {
 
     public static void main(String[] args) {
         // Crear pacientes con dietas
-        Pediatrico pediatrico = new Pediatrico("Lucía", "12345678", "OSDE - Plan Pediátrico", 12.5, "155555555", "Ana", "Dieta libre de gluten");
-        Adulto adulto = new Adulto("Carlos", "23456789", "Swiss Medical - Plan Adulto", 120, 1.75, 80);
-        Internado internado = new Internado("Jorge", "34567890", "Galeno - Plan Internación", "12A", 130, "A+", "Dieta hiposódica y controlada");
+        Pediatrico pediatrico = new Pediatrico("Lucía Something", "12345678", "OSDE - Plan Pediátrico", 12.5, "155555555", "Ana", "Dieta libre de gluten");
+        Adulto adulto = new Adulto("Carlos Carlitos", "23456789", "Swiss Medical - Plan Adulto", 120, 1.75, 80);
+        Internado internado = new Internado("Jorge Jorgeloooni", "34567890", "Galeno - Plan Internación", "12A", 130, "A+", "Dieta hiposódica y controlada");
 
         // Departamentos
         Administracion administracion = new Administracion();
